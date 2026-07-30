453b56e0547d7909e7c2803519e9ba312c8c4bf0
Yura Yurchuk <164063015+YuraYurchuk@users.noreply.github.com>
2026-07-30 20:35:19 +0300

    add several minor change for maven checkstyle plugin

--- backend/src/main/java/travelplanner/repository/TripRepository.java
+++ backend/src/main/java/travelplanner/repository/TripRepository.java
@@ -1,10 +1,10 @@
 package travelplanner.repository;
 
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import travelplanner.entity.Trip;
-import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface TripRepository extends JpaRepository<Trip, UUID> {
