1addd33338a6700c9b2b264221a4849a1c183be6
Yura Yurchuk <164063015+YuraYurchuk@users.noreply.github.com>
2026-08-04 22:04:45 +0300

    fix liquibase migration format

--- backend/src/main/resources/db/changelog/changes/06-update-places-table.sql
+++ backend/src/main/resources/db/changelog/changes/06-update-places-table.sql
@@ -1,2 +1,7 @@
+--liquibase formatted sql
+
+--changeset ihor:add-trip-id-column
 ALTER TABLE places ADD COLUMN trip_id VARCHAR(36);
+
+--changeset ihor:add-fk-trip-id
 ALTER TABLE places ADD CONSTRAINT fk_places_trip_id FOREIGN KEY (trip_id) REFERENCES trips(trip_id);
